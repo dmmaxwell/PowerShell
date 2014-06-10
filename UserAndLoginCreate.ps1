2df7804a889213eff193118a5b6a823d10ef19ae
David M Maxwell <dmmaxwell@gmail.com>
2014-06-10 14:43:51 -0400

    Uploading my first script.
    Uploading user and login create script - playing with vars.

---
+++ UserAndLoginCreate.ps1
@@ -0,0 +1,32 @@
+﻿#####################################################
+# User and login creation script. 
+# dmmaxwell - June 2014
+#
+# My first attempt at using variables in PowerShell.
+#####################################################
+
+#Set the variables that will be edited before running.
+$uname = "pstestlogin"
+$db = "tempdb"
+$server = "oemis5wk112"
+$pw = "PSTest123."
+
+#Create the T-SQL command required.
+$query = "
+CREATE LOGIN [$uname] WITH 
+    PASSWORD = '$pw', 
+    CHECK_EXPIRATION = OFF, 
+    CHECK_POLICY = ON,
+    DEFAULT_DATABASE = [$db]
+GO
+
+USE [$db];
+GO
+
+CREATE USER [$user]
+FOR LOGIN [$user];
+GO
+
+"
+#Then, run the command.
+Invoke-Sqlcmd -ServerInstance $server -Query $query
